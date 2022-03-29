ab74fe591393e35b1863ef7fba70a2c099390369
kosuke <cheki3cheki3@gmail.com>
2022-03-30 07:44:35 +0900

    first comit

--- JMSDFMeguri/ViewController.swift
+++ JMSDFMeguri/ViewController.swift
@@ -6,14 +6,36 @@
 //
 
 import UIKit
+import FirebaseStorage
+import SDWebImage
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var image: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func getImage(_ sender: Any) {
+        
+        let gsRef = Storage.storage().reference(withPath: "護衛艦/ひゅうが1.jpeg")
+        
+        gsRef.downloadURL { url, error in
+            if error != nil {
+                print("error")
+            } else {
+                self.image.sd_setImage(with: url, completed: nil)
+            }
+        }
+        print("ボタン押した")
+    }
+    
 }
 
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'JMSDFMeguri' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for JMSDFMeguri
+
+pod 'SDWebImage', '~> 5.0'
+
+end
